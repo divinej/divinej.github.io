4016fa30b26e168111e29185cbe453428573c983
Divinejustice Oko <okodivinejustice@gmail.com>
2020-06-12 04:21:28 +0100

    First change

---
+++ projects/file for zomdad v2/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $('.canvasOpen').click(function(){
+        $('.canvas-menu').toggleClass('hide');
+        return false;
+    });
+    $('.canvas-close a').click(function(){
+        $('.canvas-menu').addClass('hide');
+        return false;
+    });
+
+    
+	
+})
