7945ff4c80066f49e87a946a9de3bff52ad02f10
Divinejustice Oko <okodivinejustice@gmail.com>
2020-06-13 07:27:40 +0100

    Add files via upload

---
+++ projects/Blockstreet/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    $('.canvasOpen').click(function(){
+        $('.canvas-menu').toggleClass('hide');
+        return false;
+    });
+    $('.canvas-close a').click(function(){
+        $('.canvas-menu').addClass('hide');
+        return false;
+    });
+	
+})
