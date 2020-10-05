568eda02c1261abc4bec984705a30fbe71a5d6a6
Divinejustice Oko <okodivinejustice@gmail.com>
2020-10-05 04:52:33 -0700

    GIbik First JS files

--- projects/learn/js/script.js
+++ projects/learn/js/script.js
@@ -1,28 +1,27 @@
-let btn = document.getElementById("order");
-let elem1 = document.getElementById("detail");
-let elem2 = document.getElementById("analytics");
-let elem3 = document.getElementById("action");
+$('.carousel').carousel({
+	interval: false,
+  });
 
 
-elem1.addEventListener("click", display1);
-elem2.addEventListener("click", display2);
+  /********************************************************************
+*********** 	SCREEN 5 			 ***********************************
+********************************************************************/
+// btn1 = document.getElementById("signIn");
+// btn2 = document.getElementById("signUp");
 
-function display1() {
-	btn.style.display = 'none';
-	elem2.classList.remove("acc-details");
-	elem2.classList.add("acc-analytics");
-	elem1.classList.remove("acc-analytics");
-	elem1.classList.add("acc-details");
-	elem3.style.display = 'block';
-	document.getElementById("acc").style.display = 'block';
-}
+// btn1.addEventListener("click", changeBg);
+// btn2.addEventListener("click", changeBg2);
 
-function display2() {
-	btn.style.display ='block';
-	elem1.classList.remove("acc-details");
-	elem1.classList.add("acc-analytics");
-	elem2.classList.remove("acc-analytics");
-	elem2.classList.add("acc-details");
-	elem3.style.display = 'none';
-	document.getElementById("acc").style.display = 'none';
-}
+// function changeBg() {
+// 	btn1.classList.remove("gray-bg");
+//   btn1.classList.add("black-bg");
+//   btn2.classList.remove("black-bg");
+//   btn2.classList.add("gray-bg");
+// }
+
+// function changeBg2() {
+// 	btn2.classList.remove("gray-bg");
+//   btn2.classList.add("black-bg");
+//   btn1.classList.remove("black-bg");
+//   btn1.classList.add("gray-bg");
+// }
