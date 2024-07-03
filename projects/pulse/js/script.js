ba9df376d686efaa3e38d5842dfa6fa64b1ef497
Divinejustice Oko <okodivinejustice@gmail.com>
2024-07-02 23:24:55 -0400

    Delete projects directory

--- projects/pulse/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    
-    $('.navTrigger').click(function(){
-        // $('.mobile-menu').toggleClass('hidden');
-        $('.mobile-menu').toggleClass('is-active');
-        $('body').toggleClass('overflow');
-        return false;
-    });
-})
